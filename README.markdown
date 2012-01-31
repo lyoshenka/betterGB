0bc064f818100a6f53db501e75d0e7f042c8f5c7
Alex Grintsvayg <alex@flickswitch.cc>
2012-01-31 14:24:16 -0500

    Added option to reset options

--- README.markdown
+++ README.markdown
@@ -17,7 +17,7 @@ Features
 Install
 -------
 
-[Click here to install](https://github.com/lyoshenka/betterGB/blob/master/release/bettergb-10.crx?raw=true "Click to install")
+[Click here to install](https://github.com/lyoshenka/betterGB/blob/master/release/bettergb-11.crx?raw=true "Click to install")
 
 To configure extension options, go to the Chrome extensions page (chrome://settings/extensions) and click options next to the BetterGB extension.
 
--- src/options.js
+++ src/options.js
@@ -1,47 +1,54 @@
 $(document).ready(function() {
-  var options = chrome.extension.getBackgroundPage().getOptions();
+  var options = chrome.extension.getBackgroundPage().getOptions(),
+      load = function() {
+        $.each(options,function(option,value) {
+          input = $('#'+option);
+          if (!input)
+          {
+            return;
+          }
+          if (input.attr('type') == 'checkbox')
+          {
+            input.attr('checked', value);
+          }
+          else
+          {
+            input.val(value);
+          }
+        });
+      },
+      save = function() {
+        $.each(options, function(option,value) {
+          input = $('#'+option);
+          if (!input)
+          {
+            return;
+          }
+          if (input.attr('type') == 'checkbox')
+          {
+            localStorage[option] = input.attr('checked') == 'checked';
+          }
+          else
+          {
+            localStorage[option] = input.val();
+          }
+        });
+      };
 
-  /* load saved options into form*/
-  $.each(options,function(option,value) {
-    input = $('#'+option);
-    if (!input)
-    {
-      return;
-    }
-    if (input.attr('type') == 'checkbox')
-    {
-      console.log('Option is a checkox');
-      input.attr('checked', value);
-    }
-    else
-    {
-      console.log('Not a checkbox');
-      input.val(value);
-    }
-  });
+  load();
 
-
-
-  /* save options */
   $('#submit').click(function(e) {
     e.preventDefault();
-
-    $.each(options, function(option,value) {
-      input = $('#'+option);
-      if (!input)
-      {
-        return;
-      }
-      if (input.attr('type') == 'checkbox')
-      {
-        localStorage[option] = input.attr('checked') == 'checked';
-      }
-      else
-      {
-        localStorage[option] = input.val();
-      }
-    });
-
+    save();
     $('#saved').show().fadeOut(850);
+  });
+
+  $('#reset').click(function(e) {
+    e.preventDefault();
+    $.each(options, function(option,value) {
+      localStorage.removeItem(option);
+    });
+    options = chrome.extension.getBackgroundPage().getOptions();
+    load();
   });
 });
